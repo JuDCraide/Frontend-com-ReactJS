fbe618cf999a6b81058cbb05167c87ab905001f1
JúDCraide <juliadcraide31@gmail.com>
2020-10-29 19:23:35 -0300

    Componentização

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './components/Header'
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Header />
+            <h2>Hello World</h2>
+        </>
+    )
+}
+
+export default App;
