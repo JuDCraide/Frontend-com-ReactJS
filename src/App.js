1ebf0688a820fc98036a6bf67c9da669224d3109
JúDCraide <juliadcraide31@gmail.com>
2020-10-29 21:49:29 -0300

    Estado e imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header'
 
 function App() {
+
+    const projects = ['API com Node', 'Frontend com React']
+    const [projectsInState, setProjectsInState] = useState(['API com Node', 'Frontend com React']);
+
+    function handleAddNewProject() {
+        projects.push(`Novo projeto ${Date.now()}`);
+        setProjectsInState([...projectsInState, `Novo projeto ${Date.now()}`]);
+
+        console.log("Projects sem estado", projects);
+    }
+
     return (
         <>
-            <Header title="Home Page">
-                <ul>
-                    <li>Home Page</li>
-                    <li>Projects</li>
-                    <li>Login</li>
-                </ul>
-            </Header>
-            <Header title="Projects">
-                <ul>
-                    <li>API com Node</li>
-                    <li>Frontend com React</li>
-                </ul>
-            </Header>
-            <h2>Hello World</h2>
+            <Header title="Home Page" />
+
+            <ul>{
+                projects.map((project, index) =>
+                    <li key={index}>{project}</li>
+                )
+            }</ul>
+
+            <ul>{
+                projectsInState.map((project, index) =>
+                    <li key={index}>{project}</li>
+                )
+            }</ul>
+
+            <button
+                type="button" onClick={handleAddNewProject}>Adicionar projetos</button>
         </>
     )
 }
